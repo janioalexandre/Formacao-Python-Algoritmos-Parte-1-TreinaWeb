793a67656b16fd4f34a28abd7edd66d3ad8d72be
Janio Alexandre Ribeiro <janioalexandre@gmail.com>
2025-03-04 23:08:48 -0300

    Algoritmos de busca -  busca binária

--- main.py
+++ main.py
@@ -36,3 +36,30 @@ for i in range(tamanho):
     print("vetor: ", numeros)   
 
 # FIM SELECT SORT
+
+# BUSCA BINÁRIA
+# 2
+# 1 | 2 | 3 | 4 | 5
+# 1 | 2 
+# 2
+
+resultado = -1
+inicio = 0
+fim = tamanho - 1
+meio = 0
+alvo = int(input("Digite o elemento a ser pesquisado no vetor: "))
+while inicio <= fim:
+    meio = int((inicio + fim) / 2)
+    if (numeros[meio] < alvo):
+        inicio = meio + 1
+    elif (numeros[meio] > alvo):
+        fim = meio - 1
+    else:
+        resultado = meio
+        break
+if resultado < 0:
+    print("Elemento não encontrado no vetor")
+else:
+    print(f"O elemento {alvo} está na posição {resultado}")
+
+# FIM BUSCA BINÁRIA
